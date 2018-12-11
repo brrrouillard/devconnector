3da21eb3a6bb3b4cdb7c4a7dcd9bd2f055979964
Matthieu Brouillard <mbrouilla@gmail.com>
2018-12-11 09:27:33 +0100

    Add jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const passport = require("passport");
 
+const keys = require('../../config/keys');
 const User = require("../../models/User");
 
 // @routes  GET api/users/test
@@ -69,7 +72,20 @@ router.post("/login", (req, res) => {
         // Check password
         bcrypt.compare(password, user.password).then(isMatch => {
             if (isMatch) {
-                res.json({ msg: "Success" });
+                // User matched
+                // Create JWT Payload
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    avatar: user.avatar
+                }
+                // Sign token
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600}, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    });
+                });
             } else {
                 return res.status(400).json({ password: "Password incorrect" });
             }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -9,7 +9,7 @@ const posts = require("./routes/api/posts");
 const app = express();
 
 // Body Parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
@@ -17,19 +17,17 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-app.get("/", (req, res) => res.send("Hello"));
+    .connect(
+        db,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
 
 // Use routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const PORT = process.env.PORT || 5000;
 
